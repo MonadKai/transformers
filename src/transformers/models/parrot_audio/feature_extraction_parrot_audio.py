f028b97704bc64a28592ca1aceef7b8a00f7d748
kai.liu <kai.liu@brgroup.com>
2025-07-26 02:53:53 -0700

    :art: Use `parrot_sensevoice` in `parrot_audio`

--- src/transformers/models/parrot_audio/feature_extraction_parrot_audio.py
+++ src/transformers/models/parrot_audio/feature_extraction_parrot_audio.py
@@ -22,6 +22,7 @@ import torchaudio.compliance.kaldi as kaldi
 from torch.nn.utils.rnn import pad_sequence
 
 from transformers.feature_extraction_utils import BatchFeature, FeatureExtractionMixin
+from transformers.models.parrot_sensevoice.feature_extraction_parrot_sensevoice import ParrotSenseVoiceFeatureExtractor
 
 
 def apply_cmvn(inputs: torch.Tensor, means: torch.Tensor, vars: torch.Tensor) -> torch.Tensor:
@@ -49,99 +50,99 @@ def apply_lfr(inputs: torch.Tensor, lfr_m: int, lfr_n: int) -> torch.Tensor:
     return LFR_outputs.clone().type(torch.float32)
 
 
-# TODO: direct use ParrotSenseVoiceFeatureExtractor, skip this class
-class ParrotAudioFeatureExtractor(FeatureExtractionMixin):
-    def __init__(self, 
-        means: list[float],
-        vars: list[float],
-        cmvn_file: Optional[str] = None,
-        fs: int = 16000,
-        window: str = "hamming",
-        n_mels: int = 80,
-        frame_length: int = 25,
-        frame_shift: int = 10,
-        filter_length_min: int = -1,
-        filter_length_max: int = -1,
-        lfr_m: int = 7,
-        lfr_n: int = 6,
-        dither: float = 0.0,
-        snip_edges: bool = True,
-        upsacle_samples: bool = True,
-        **kwargs,
-    ):
-        super().__init__(
-            cmvn_file=cmvn_file,
-            means=means,
-            vars=vars,
-            fs=fs,
-            window=window,
-            n_mels=n_mels,
-            frame_length=frame_length,
-            frame_shift=frame_shift,
-            filter_length_min=filter_length_min,
-            filter_length_max=filter_length_max,
-            lfr_m=lfr_m,
-            lfr_n=lfr_n,
-            dither=dither,
-            snip_edges=snip_edges,
-            upsacle_samples=upsacle_samples,
-            **kwargs
-        )
+# class ParrotAudioFeatureExtractor(FeatureExtractionMixin):
+#     def __init__(self, 
+#         means: list[float],
+#         vars: list[float],
+#         cmvn_file: Optional[str] = None,
+#         fs: int = 16000,
+#         window: str = "hamming",
+#         n_mels: int = 80,
+#         frame_length: int = 25,
+#         frame_shift: int = 10,
+#         filter_length_min: int = -1,
+#         filter_length_max: int = -1,
+#         lfr_m: int = 7,
+#         lfr_n: int = 6,
+#         dither: float = 0.0,
+#         snip_edges: bool = True,
+#         upsacle_samples: bool = True,
+#         **kwargs,
+#     ):
+#         super().__init__(
+#             cmvn_file=cmvn_file,
+#             means=means,
+#             vars=vars,
+#             fs=fs,
+#             window=window,
+#             n_mels=n_mels,
+#             frame_length=frame_length,
+#             frame_shift=frame_shift,
+#             filter_length_min=filter_length_min,
+#             filter_length_max=filter_length_max,
+#             lfr_m=lfr_m,
+#             lfr_n=lfr_n,
+#             dither=dither,
+#             snip_edges=snip_edges,
+#             upsacle_samples=upsacle_samples,
+#             **kwargs
+#         )
 
-    @cached_property
-    def _means(self) -> torch.Tensor:
-        return torch.as_tensor(self.means, dtype=torch.float32)
+#     @cached_property
+#     def _means(self) -> torch.Tensor:
+#         return torch.as_tensor(self.means, dtype=torch.float32)
 
-    @cached_property
-    def _vars(self) -> torch.Tensor:
-        return torch.as_tensor(self.vars, dtype=torch.float32)
+#     @cached_property
+#     def _vars(self) -> torch.Tensor:
+#         return torch.as_tensor(self.vars, dtype=torch.float32)
 
-    @torch.no_grad()
-    def __call__(self,
-            inputs: list[torch.Tensor],
-            input_lengths: list[int],
-            **kwargs,
-    ) -> BatchFeature:
-        # input_lengths = [i.shape[0] for i in inputs]
-        batch_size = len(inputs)
-        feats = []
-        feats_lens = []
-        for i in range(batch_size):
-            waveform_length = input_lengths[i]
-            waveform = inputs[i][:waveform_length]
-            if self.upsacle_samples:
-                waveform = waveform * (1 << 15)
-            if isinstance(waveform, np.ndarray):
-                waveform = torch.from_numpy(waveform)
-            waveform = waveform.unsqueeze(0)
-            mat = kaldi.fbank(
-                waveform,
-                num_mel_bins=self.n_mels,
-                frame_length=min(self.frame_length, waveform_length / self.fs * 1000),
-                frame_shift=self.frame_shift,
-                dither=self.dither,
-                energy_floor=0.0,
-                window_type=self.window,
-                sample_frequency=self.fs,
-                snip_edges=self.snip_edges,
-            )  # [T, n_mels]
+#     @torch.no_grad()
+#     def __call__(self,
+#             inputs: list[torch.Tensor],
+#             input_lengths: list[int],
+#             **kwargs,
+#     ) -> BatchFeature:
+#         # input_lengths = [i.shape[0] for i in inputs]
+#         batch_size = len(inputs)
+#         feats = []
+#         feats_lens = []
+#         for i in range(batch_size):
+#             waveform_length = input_lengths[i]
+#             waveform = inputs[i][:waveform_length]
+#             if self.upsacle_samples:
+#                 waveform = waveform * (1 << 15)
+#             if isinstance(waveform, np.ndarray):
+#                 waveform = torch.from_numpy(waveform)
+#             waveform = waveform.unsqueeze(0)
+#             mat = kaldi.fbank(
+#                 waveform,
+#                 num_mel_bins=self.n_mels,
+#                 frame_length=min(self.frame_length, waveform_length / self.fs * 1000),
+#                 frame_shift=self.frame_shift,
+#                 dither=self.dither,
+#                 energy_floor=0.0,
+#                 window_type=self.window,
+#                 sample_frequency=self.fs,
+#                 snip_edges=self.snip_edges,
+#             )  # [T, n_mels]
 
-            if self.lfr_m != 1 or self.lfr_n != 1:
-                mat = apply_lfr(mat, self.lfr_m, self.lfr_n)
-            mat = apply_cmvn(mat, self._means, self._vars)  # [feature_length, n_mels * 7]
-            feat_length = mat.size(0)
-            feats.append(mat)
-            feats_lens.append(feat_length)
+#             if self.lfr_m != 1 or self.lfr_n != 1:
+#                 mat = apply_lfr(mat, self.lfr_m, self.lfr_n)
+#             mat = apply_cmvn(mat, self._means, self._vars)  # [feature_length, n_mels * 7]
+#             feat_length = mat.size(0)
+#             feats.append(mat)
+#             feats_lens.append(feat_length)
 
-        feats_lens = torch.as_tensor(feats_lens)  # [batch_size]
-        max_len = feats_lens.max().item()
-        idxs = torch.arange(max_len).expand(feats_lens.size(0), max_len)  # [batch_size, max_len]
-        feature_attention_masks = idxs < feats_lens.unsqueeze(1)  # [batch_size, max_len]
-        if batch_size == 1:
-            feats_pad = feats[0][None, :, :]  # [1, feature_length, n_mels * 7]
-        else:
-            feats_pad = pad_sequence(feats, batch_first=True, padding_value=0.0)  # [batch_size, feature_length, n_mels * 7]
-        return {'input_features': feats_pad, 'attention_mask': feature_attention_masks}
+#         feats_lens = torch.as_tensor(feats_lens)  # [batch_size]
+#         max_len = feats_lens.max().item()
+#         idxs = torch.arange(max_len).expand(feats_lens.size(0), max_len)  # [batch_size, max_len]
+#         feature_attention_masks = idxs < feats_lens.unsqueeze(1)  # [batch_size, max_len]
+#         if batch_size == 1:
+#             feats_pad = feats[0][None, :, :]  # [1, feature_length, n_mels * 7]
+#         else:
+#             feats_pad = pad_sequence(feats, batch_first=True, padding_value=0.0)  # [batch_size, feature_length, n_mels * 7]
+#         return {'input_features': feats_pad, 'attention_mask': feature_attention_masks}
 
+ParrotAudioFeatureExtractor = ParrotSenseVoiceFeatureExtractor
 
 __all__ = ["ParrotAudioFeatureExtractor"]
--- src/transformers/models/parrot_audio/modeling_parrot_audio.py
+++ src/transformers/models/parrot_audio/modeling_parrot_audio.py
@@ -25,6 +25,10 @@ from transformers.cache_utils import Cache
 from transformers.generation import GenerationMixin
 from transformers.modeling_outputs import BaseModelOutput, ModelOutput
 from transformers.modeling_utils import PreTrainedModel
+from transformers.models.parrot_sensevoice.modeling_parrot_sensevoice import (
+    ParrotSenseVoiceEncoder,
+    ParrotSenseVoicePreTrainedModel,
+)
 from transformers.models.qwen2.modeling_qwen2 import Qwen2ForCausalLM
 from transformers.utils import (
     add_start_docstrings,
@@ -34,7 +38,9 @@ from transformers.utils import (
 )
 
 from .configuration_parrot_audio import ParrotAudioConfig, ParrotAudioEncoderConfig
-from .sensevoice_encoder import SenseVoiceEncoderSmall
+
+
+# from .sensevoice_encoder import SenseVoiceEncoderSmall
 
 
 logger = logging.get_logger(__name__)
@@ -84,134 +90,136 @@ class ParrotAudioCausalLMOutputWithPast(ModelOutput):
     attention_mask: Optional[torch.FloatTensor] = None
 
 
-PARROTAUDIO_START_DOCSTRING = r"""
-    This model inherits from [`PreTrainedModel`]. Check the superclass documentation for the generic methods the
-    library implements for all its model (such as downloading or saving, resizing the input embeddings, pruning heads
-    etc.)
+# PARROTAUDIO_START_DOCSTRING = r"""
+#     This model inherits from [`PreTrainedModel`]. Check the superclass documentation for the generic methods the
+#     library implements for all its model (such as downloading or saving, resizing the input embeddings, pruning heads
+#     etc.)
 
-    This model is also a PyTorch [torch.nn.Module](https://pytorch.org/docs/stable/nn.html#torch.nn.Module) subclass.
-    Use it as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to general usage
-    and behavior.
+#     This model is also a PyTorch [torch.nn.Module](https://pytorch.org/docs/stable/nn.html#torch.nn.Module) subclass.
+#     Use it as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to general usage
+#     and behavior.
 
-    Parameters:
-        config ([`ParrotAudioConfig`]):
-            Model configuration class with all the parameters of the model. Initializing with a config file does not
-            load the weights associated with the model, only the configuration. Check out the
-            [`~PreTrainedModel.from_pretrained`] method to load the model weights.
-"""
-
-
-@add_start_docstrings(
-    "The bare ParrotAudio Model outputting raw hidden-states without any specific head on top.",
-    PARROTAUDIO_START_DOCSTRING,
-)
-class ParrotAudioPreTrainedModel(PreTrainedModel):
-    config_class = ParrotAudioConfig
-    # TODO: check this prefix
-    # base_model_prefix = "model"
-    base_model_prefix = "sense_voice_small"
-    supports_gradient_checkpointing = True
-    _skip_keys_device_placement = "past_key_values"
-    _supports_flash_attn_2 = True
-    _supports_sdpa = True
-    _supports_attention_backend = False
-
-    def _init_weights(self, module):
-        # important: this ported version of ParrotAudio isn't meant for training from scratch - only
-        # inference and fine-tuning - so the proper init weights code has been removed
-        std = self.config.init_std if hasattr(self.config, "init_std") else self.config.audio_config.init_std
-
-        if isinstance(module, (nn.Linear, nn.Conv1d)):
-            module.weight.data.normal_(mean=0.0, std=std)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=std)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
+#     Parameters:
+#         config ([`ParrotAudioConfig`]):
+#             Model configuration class with all the parameters of the model. Initializing with a config file does not
+#             load the weights associated with the model, only the configuration. Check out the
+#             [`~PreTrainedModel.from_pretrained`] method to load the model weights.
+# """
 
 
-PARROTAUDIOENCODER_START_DOCSTRING = r"""
-    This model inherits from [`PreTrainedModel`]. Check the superclass documentation for the generic methods the
-    library implements for all its model (such as downloading or saving, resizing the input embeddings, pruning heads
-    etc.)
+# @add_start_docstrings(
+#     "The bare ParrotAudio Model outputting raw hidden-states without any specific head on top.",
+#     PARROTAUDIO_START_DOCSTRING,
+# )
+# class ParrotAudioPreTrainedModel(PreTrainedModel):
+#     config_class = ParrotAudioConfig
+#     # TODO: check this prefix
+#     # base_model_prefix = "model"
+#     base_model_prefix = "sense_voice_small"
+#     supports_gradient_checkpointing = True
+#     _skip_keys_device_placement = "past_key_values"
+#     _supports_flash_attn_2 = True
+#     _supports_sdpa = True
+#     _supports_attention_backend = False
 
-    This model is also a PyTorch [torch.nn.Module](https://pytorch.org/docs/stable/nn.html#torch.nn.Module) subclass.
-    Use it as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to general usage
-    and behavior.
+#     def _init_weights(self, module):
+#         # important: this ported version of ParrotAudio isn't meant for training from scratch - only
+#         # inference and fine-tuning - so the proper init weights code has been removed
+#         std = self.config.init_std if hasattr(self.config, "init_std") else self.config.audio_config.init_std
 
-    Parameters:
-        config ([`ParrotAudioEncoderConfig`]):
-            Model configuration class with all the parameters of the model. Initializing with a config file does not
-            load the weights associated with the model, only the configuration. Check out the
-            [`~PreTrainedModel.from_pretrained`] method to load the model weights.
-"""
+#         if isinstance(module, (nn.Linear, nn.Conv1d)):
+#             module.weight.data.normal_(mean=0.0, std=std)
+#             if module.bias is not None:
+#                 module.bias.data.zero_()
+#         elif isinstance(module, nn.Embedding):
+#             module.weight.data.normal_(mean=0.0, std=std)
+#             if module.padding_idx is not None:
+#                 module.weight.data[module.padding_idx].zero_()
 
 
-# TODO: add comment to hint this class is copied from ParrotSenseVoiceEncoder
-@add_start_docstrings(
-    """The audio model from ParrotAudio without any head or projection on top.""",
-    PARROTAUDIOENCODER_START_DOCSTRING,
-)
-class ParrotAudioEncoder(ParrotAudioPreTrainedModel):
-    """
-    Transformer encoder consisting of *config.encoder_layers* self attention layers. Each layer is a
-    [`ParrotAudioEncoderLayer`].
+# PARROTAUDIOENCODER_START_DOCSTRING = r"""
+#     This model inherits from [`PreTrainedModel`]. Check the superclass documentation for the generic methods the
+#     library implements for all its model (such as downloading or saving, resizing the input embeddings, pruning heads
+#     etc.)
 
-    Args:
-        config: ParrotAudioEncoderConfig
-    """
+#     This model is also a PyTorch [torch.nn.Module](https://pytorch.org/docs/stable/nn.html#torch.nn.Module) subclass.
+#     Use it as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to general usage
+#     and behavior.
 
-    # Ignore copy
-    config_class = ParrotAudioEncoderConfig
-    main_input_name = "input_features"
-    _no_split_modules = ["EncoderLayerSANM"]
+#     Parameters:
+#         config ([`ParrotAudioEncoderConfig`]):
+#             Model configuration class with all the parameters of the model. Initializing with a config file does not
+#             load the weights associated with the model, only the configuration. Check out the
+#             [`~PreTrainedModel.from_pretrained`] method to load the model weights.
+# """
 
-    def __init__(self, config: ParrotAudioEncoderConfig):
-        super().__init__(config)
-        self.sense_voice_small = SenseVoiceEncoderSmall(
-            input_size=config.input_size,
-            output_size=config.output_size,
-            attention_heads=config.attention_heads,
-            linear_units=config.linear_units,
-            num_blocks=config.num_blocks,
-            tp_blocks=config.tp_blocks,
-            dropout_rate=config.dropout_rate,
-            attention_dropout_rate=config.attention_dropout_rate,
-            normalize_before=config.normalize_before,
-            kernel_size=config.kernel_size,
-            sanm_shfit=config.sanm_shfit
-        )
 
-    def _freeze_parameters(self):
-        for param in self.parameters():
-            param.requires_grad = False
-        self._requires_grad = False
+# # TODO: add comment to hint this class is copied from ParrotSenseVoiceEncoder
+# @add_start_docstrings(
+#     """The audio model from ParrotAudio without any head or projection on top.""",
+#     PARROTAUDIOENCODER_START_DOCSTRING,
+# )
+# class ParrotAudioEncoder(ParrotAudioPreTrainedModel):
+#     """
+#     Transformer encoder consisting of *config.encoder_layers* self attention layers. Each layer is a
+#     [`ParrotAudioEncoderLayer`].
 
-    # TODO: refine output type to BaseModelOutput
-    def forward(
-        self,
-        input_features: torch.Tensor,
-        attention_mask: Optional[torch.Tensor] = None,
-        audio_feature_lengths: Optional[torch.Tensor] = None,
-        head_mask: Optional[torch.Tensor] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
-        return_dict: Optional[bool] = None,
-    ) -> tuple[torch.Tensor, torch.Tensor]:
-        xs_pad, olens, _ = self.sense_voice_small(
-            input_features,
-            ilens = audio_feature_lengths
-        )
-        return xs_pad, olens
+#     Args:
+#         config: ParrotAudioEncoderConfig
+#     """
 
-    # Ignore copy
-    def _get_feat_extract_output_lengths(self, input_lengths: torch.LongTensor) -> tuple[torch.LongTensor, torch.LongTensor]:
-        """
-        Computes the output length of the parrot audio encoder
-        """
-        return input_lengths, input_lengths
+#     # Ignore copy
+#     config_class = ParrotAudioEncoderConfig
+#     main_input_name = "input_features"
+#     _no_split_modules = ["EncoderLayerSANM"]
 
+#     def __init__(self, config: ParrotAudioEncoderConfig):
+#         super().__init__(config)
+#         self.sense_voice_small = SenseVoiceEncoderSmall(
+#             input_size=config.input_size,
+#             output_size=config.output_size,
+#             attention_heads=config.attention_heads,
+#             linear_units=config.linear_units,
+#             num_blocks=config.num_blocks,
+#             tp_blocks=config.tp_blocks,
+#             dropout_rate=config.dropout_rate,
+#             attention_dropout_rate=config.attention_dropout_rate,
+#             normalize_before=config.normalize_before,
+#             kernel_size=config.kernel_size,
+#             sanm_shfit=config.sanm_shfit
+#         )
+
+#     def _freeze_parameters(self):
+#         for param in self.parameters():
+#             param.requires_grad = False
+#         self._requires_grad = False
+
+#     # TODO: refine output type to BaseModelOutput
+#     def forward(
+#         self,
+#         input_features: torch.Tensor,
+#         attention_mask: Optional[torch.Tensor] = None,
+#         audio_feature_lengths: Optional[torch.Tensor] = None,
+#         head_mask: Optional[torch.Tensor] = None,
+#         output_attentions: Optional[bool] = None,
+#         output_hidden_states: Optional[bool] = None,
+#         return_dict: Optional[bool] = None,
+#     ) -> tuple[torch.Tensor, torch.Tensor]:
+#         xs_pad, olens, _ = self.sense_voice_small(
+#             input_features,
+#             ilens = audio_feature_lengths
+#         )
+#         return xs_pad, olens
+
+#     # Ignore copy
+#     def _get_feat_extract_output_lengths(self, input_lengths: torch.LongTensor) -> tuple[torch.LongTensor, torch.LongTensor]:
+#         """
+#         Computes the output length of the parrot audio encoder
+#         """
+#         return input_lengths, input_lengths
+
+ParrotAudioPreTrainedModel = ParrotSenseVoicePreTrainedModel
+ParrotAudioEncoder = ParrotSenseVoiceEncoder
 
 class LinearAdaptor(nn.Module):
     def __init__(self, encoder_dim: int, ffn_dim: int, llm_dim: int, **kwargs):
