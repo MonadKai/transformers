b3a678c07fdd5c19c6232aee194b661ebc21ff89
kai.liu <kai.liu@brgroup.com>
2025-07-26 04:17:47 -0700

    :art: Use `parrot_sensevoice` in `parrot2_audio`

--- src/transformers/models/parrot2_audio/configuration_parrot2_audio.py
+++ src/transformers/models/parrot2_audio/configuration_parrot2_audio.py
@@ -14,115 +14,120 @@
 """Parrot2Audio model configuration"""
 
 from transformers.configuration_utils import PretrainedConfig
+from transformers.models.parrot_sensevoice.configuration_parrot_sensevoice import ParrotSenseVoiceConfig
 from transformers.models.qwen3 import Qwen3Config
 from transformers.utils import logging
+
 
 logger = logging.get_logger(__name__)
 
 
-class Parrot2AudioEncoderConfig(PretrainedConfig):
-    r"""
-    This is the configuration class to store the configuration of a [`Parrot2AudioEncoder`]. It is used to instantiate a
-    Parrot2Audio audio encoder according to the specified arguments, defining the model architecture. Instantiating a
-    configuration with the defaults will yield a similar configuration to that of the audio encoder of the Parrot2Audio
-    architecture.
+# class Parrot2AudioEncoderConfig(PretrainedConfig):
+#     r"""
+#     This is the configuration class to store the configuration of a [`Parrot2AudioEncoder`]. It is used to instantiate a
+#     Parrot2Audio audio encoder according to the specified arguments, defining the model architecture. Instantiating a
+#     configuration with the defaults will yield a similar configuration to that of the audio encoder of the Parrot2Audio
+#     architecture.
 
-    e.g. [bairong-inc/Parrot2Audio-14B-QA](https://huggingface.co/bairong-inc/Parrot2Audio-14B-QA)
+#     e.g. [bairong-inc/Parrot2Audio-14B-QA](https://huggingface.co/bairong-inc/Parrot2Audio-14B-QA)
 
-    Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
-    documentation from [`PretrainedConfig`] for more information.
+#     Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
+#     documentation from [`PretrainedConfig`] for more information.
 
-    Args:
-        attention_dropout_rate (`float`, *optional*, defaults to 0.1):
-            The dropout ratio for the attention probabilities.
-        attention_heads (`int`, *optional*, defaults to 4):
-            Number of attention heads for each attention layer in the Transformer encoder.
-        dropout_rate (`float`, *optional*, defaults to 0.1):
-            The dropout probability for all fully connected layers in the embeddings, encoder, and pooler.
-        input_layer (`str`, *optional*, defaults to 'pe'):
-            The type of input layer to use.
-        input_size (`int`, *optional*, defaults to 560):
-            The size of the input features.
-        kernel_size (`int`, *optional*, defaults to 11):
-            The kernel size for the convolutional layers.
-        linear_units (`int`, *optional*, defaults to 2048):
-            The number of units in the linear layers.
-        normalize_before (`bool`, *optional*, defaults to True):
-            Whether to normalize the input before the first layer.
-        num_blocks (`int`, *optional*, defaults to 50):
-            The number of blocks in the encoder.
-        output_size (`int`, *optional*, defaults to 512):
-            The size of the output features.
-        pos_enc_class (`str`, *optional*, defaults to 'SinusoidalPositionEncoder'):
-            The class of the position encoder.
-        positional_dropout_rate (`float`, *optional*, defaults to 0.1):
-            The dropout ratio for the positional encodings.
-        sanm_shfit (`int`, *optional*, defaults to 0):
-            The shift size for the SANM layers.
-        selfattention_layer_type (`str`, *optional*, defaults to 'sanm'):
-            The type of self-attention layer to use.
-        tp_blocks (`int`, *optional*, defaults to 20):
-            The number of blocks in the Transformer encoder.
-        adaptor_ffn_dim (`int`, *optional*, defaults to 8192):
-            The number of units in the feed-forward network.
+#     Args:
+#         attention_dropout_rate (`float`, *optional*, defaults to 0.1):
+#             The dropout ratio for the attention probabilities.
+#         attention_heads (`int`, *optional*, defaults to 4):
+#             Number of attention heads for each attention layer in the Transformer encoder.
+#         dropout_rate (`float`, *optional*, defaults to 0.1):
+#             The dropout probability for all fully connected layers in the embeddings, encoder, and pooler.
+#         input_layer (`str`, *optional*, defaults to 'pe'):
+#             The type of input layer to use.
+#         input_size (`int`, *optional*, defaults to 560):
+#             The size of the input features.
+#         kernel_size (`int`, *optional*, defaults to 11):
+#             The kernel size for the convolutional layers.
+#         linear_units (`int`, *optional*, defaults to 2048):
+#             The number of units in the linear layers.
+#         normalize_before (`bool`, *optional*, defaults to True):
+#             Whether to normalize the input before the first layer.
+#         num_blocks (`int`, *optional*, defaults to 50):
+#             The number of blocks in the encoder.
+#         output_size (`int`, *optional*, defaults to 512):
+#             The size of the output features.
+#         pos_enc_class (`str`, *optional*, defaults to 'SinusoidalPositionEncoder'):
+#             The class of the position encoder.
+#         positional_dropout_rate (`float`, *optional*, defaults to 0.1):
+#             The dropout ratio for the positional encodings.
+#         sanm_shfit (`int`, *optional*, defaults to 0):
+#             The shift size for the SANM layers.
+#         selfattention_layer_type (`str`, *optional*, defaults to 'sanm'):
+#             The type of self-attention layer to use.
+#         tp_blocks (`int`, *optional*, defaults to 20):
+#             The number of blocks in the Transformer encoder.
+#         adaptor_ffn_dim (`int`, *optional*, defaults to 8192):
+#             The number of units in the feed-forward network.
 
-    Example:
+#     Example:
 
-    ```python
-    >>> from transformers import Parrot2AudioEncoderConfig, Parrot2AudioEncoder
+#     ```python
+#     >>> from transformers import Parrot2AudioEncoderConfig, Parrot2AudioEncoder
 
-    >>> # Initializing a Parrot2AudioEncoderConfig
-    >>> configuration = Parrot2AudioEncoderConfig()
+#     >>> # Initializing a Parrot2AudioEncoderConfig
+#     >>> configuration = Parrot2AudioEncoderConfig()
 
-    >>> # Initializing a ParrotAudioEncoder (with random weights)
-    >>> model = Parrot2AudioEncoder(configuration)
+#     >>> # Initializing a ParrotAudioEncoder (with random weights)
+#     >>> model = Parrot2AudioEncoder(configuration)
 
-    >>> # Accessing the model configuration
-    >>> configuration = model.config
-    ```"""
+#     >>> # Accessing the model configuration
+#     >>> configuration = model.config
+#     ```"""
 
-    model_type = "parrot2_audio_encoder"
+#     model_type = "parrot2_audio_encoder"
 
-    def __init__(
-        self,
-        # occurs in config.json `audio_config`
-        attention_dropout_rate=0.1,
-        attention_heads=4,
-        dropout_rate=0.1,
-        input_layer='pe',
-        input_size=560,
-        kernel_size=11,
-        linear_units=2048,
-        normalize_before=True,
-        num_blocks=50,
-        output_size=512,
-        pos_enc_class='SinusoidalPositionEncoder',
-        positional_dropout_rate=0.1,
-        sanm_shfit=0,
-        selfattention_layer_type='sanm',
-        tp_blocks=20,
-        init_std=0.02,
-        # not in config.json `audio_config`
-        **kwargs,
-    ):
-        super().__init__(**kwargs)
+#     def __init__(
+#         self,
+#         # occurs in config.json `audio_config`
+#         attention_dropout_rate=0.1,
+#         attention_heads=4,
+#         dropout_rate=0.1,
+#         input_layer='pe',
+#         input_size=560,
+#         kernel_size=11,
+#         linear_units=2048,
+#         normalize_before=True,
+#         num_blocks=50,
+#         output_size=512,
+#         pos_enc_class='SinusoidalPositionEncoder',
+#         positional_dropout_rate=0.1,
+#         sanm_shfit=0,
+#         selfattention_layer_type='sanm',
+#         tp_blocks=20,
+#         init_std=0.02,
+#         # not in config.json `audio_config`
+#         **kwargs,
+#     ):
+#         super().__init__(**kwargs)
 
-        self.attention_dropout_rate = attention_dropout_rate
-        self.attention_heads = attention_heads
-        self.dropout_rate = dropout_rate
-        self.input_layer = input_layer
-        self.input_size = input_size
-        self.kernel_size = kernel_size
-        self.linear_units = linear_units
-        self.normalize_before = normalize_before
-        self.num_blocks = num_blocks
-        self.output_size = output_size
-        self.pos_enc_class = pos_enc_class
-        self.positional_dropout_rate = positional_dropout_rate
-        self.sanm_shfit = sanm_shfit
-        self.selfattention_layer_type = selfattention_layer_type
-        self.tp_blocks = tp_blocks
-        self.init_std = init_std
+#         self.attention_dropout_rate = attention_dropout_rate
+#         self.attention_heads = attention_heads
+#         self.dropout_rate = dropout_rate
+#         self.input_layer = input_layer
+#         self.input_size = input_size
+#         self.kernel_size = kernel_size
+#         self.linear_units = linear_units
+#         self.normalize_before = normalize_before
+#         self.num_blocks = num_blocks
+#         self.output_size = output_size
+#         self.pos_enc_class = pos_enc_class
+#         self.positional_dropout_rate = positional_dropout_rate
+#         self.sanm_shfit = sanm_shfit
+#         self.selfattention_layer_type = selfattention_layer_type
+#         self.tp_blocks = tp_blocks
+#         self.init_std = init_std
+
+
+Parrot2AudioEncoderConfig = ParrotSenseVoiceConfig
 
 
 class Parrot2AudioConfig(PretrainedConfig):
--- src/transformers/models/parrot2_audio/modeling_parrot2_audio.py
+++ src/transformers/models/parrot2_audio/modeling_parrot2_audio.py
@@ -20,19 +20,25 @@ from typing import List, Optional, Tuple, Union
 import torch
 import torch.utils.checkpoint
 from torch import nn
+
 from transformers.cache_utils import Cache
 from transformers.generation import GenerationMixin
-from transformers.modeling_outputs import ModelOutput, BaseModelOutput
+from transformers.modeling_outputs import BaseModelOutput, ModelOutput
 from transformers.modeling_utils import PreTrainedModel
+from transformers.models.parrot_audio.modeling_parrot_audio import LinearAdaptor
+from transformers.models.parrot_sensevoice.modeling_parrot_sensevoice import (
+    ParrotSenseVoiceEncoder,
+    ParrotSenseVoicePreTrainedModel,
+)
 from transformers.models.qwen3.modeling_qwen3 import Qwen3ForCausalLM
-from transformers.utils import (add_start_docstrings,
-                                add_start_docstrings_to_model_forward,
-                                logging,
-                                replace_return_docstrings)
+from transformers.utils import (
+    add_start_docstrings,
+    add_start_docstrings_to_model_forward,
+    logging,
+    replace_return_docstrings,
+)
 
-
-from .configuration_parrot2_audio import (Parrot2AudioConfig,
-                                         Parrot2AudioEncoderConfig)
+from .configuration_parrot2_audio import Parrot2AudioConfig, Parrot2AudioEncoderConfig
 from .sensevoice_encoder import SenseVoiceEncoderSmall
 
 
@@ -83,148 +89,136 @@ class Parrot2AudioCausalLMOutputWithPast(ModelOutput):
     attention_mask: Optional[torch.FloatTensor] = None
 
 
-PARROT2AUDIO_START_DOCSTRING = r"""
-    This model inherits from [`PreTrainedModel`]. Check the superclass documentation for the generic methods the
-    library implements for all its model (such as downloading or saving, resizing the input embeddings, pruning heads
-    etc.)
+# PARROT2AUDIO_START_DOCSTRING = r"""
+#     This model inherits from [`PreTrainedModel`]. Check the superclass documentation for the generic methods the
+#     library implements for all its model (such as downloading or saving, resizing the input embeddings, pruning heads
+#     etc.)
 
-    This model is also a PyTorch [torch.nn.Module](https://pytorch.org/docs/stable/nn.html#torch.nn.Module) subclass.
-    Use it as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to general usage
-    and behavior.
+#     This model is also a PyTorch [torch.nn.Module](https://pytorch.org/docs/stable/nn.html#torch.nn.Module) subclass.
+#     Use it as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to general usage
+#     and behavior.
 
-    Parameters:
-        config ([`Parrot2AudioConfig`]):
-            Model configuration class with all the parameters of the model. Initializing with a config file does not
-            load the weights associated with the model, only the configuration. Check out the
-            [`~PreTrainedModel.from_pretrained`] method to load the model weights.
-"""
-
-
-@add_start_docstrings(
-    "The bare Parrot2Audio Model outputting raw hidden-states without any specific head on top.",
-    PARROT2AUDIO_START_DOCSTRING,
-)
-class Parrot2AudioPreTrainedModel(PreTrainedModel):
-    config_class = Parrot2AudioConfig
-    # TODO: check this prefix
-    base_model_prefix = "model"
-    supports_gradient_checkpointing = True
-    _skip_keys_device_placement = "past_key_values"
-    _supports_flash_attn_2 = True
-    _supports_sdpa = True
-    _supports_attention_backend = False
-
-    def _init_weights(self, module):
-        # important: this ported version of Parrot2Audio isn't meant for training from scratch - only
-        # inference and fine-tuning - so the proper init weights code has been removed
-        std = self.config.init_std if hasattr(self.config, "init_std") else self.config.audio_config.init_std
-
-        if isinstance(module, (nn.Linear, nn.Conv1d)):
-            module.weight.data.normal_(mean=0.0, std=std)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=std)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
+#     Parameters:
+#         config ([`Parrot2AudioConfig`]):
+#             Model configuration class with all the parameters of the model. Initializing with a config file does not
+#             load the weights associated with the model, only the configuration. Check out the
+#             [`~PreTrainedModel.from_pretrained`] method to load the model weights.
+# """
 
 
-PARROT2AUDIOENCODER_START_DOCSTRING = r"""
-    This model inherits from [`PreTrainedModel`]. Check the superclass documentation for the generic methods the
-    library implements for all its model (such as downloading or saving, resizing the input embeddings, pruning heads
-    etc.)
+# @add_start_docstrings(
+#     "The bare Parrot2Audio Model outputting raw hidden-states without any specific head on top.",
+#     PARROT2AUDIO_START_DOCSTRING,
+# )
+# class Parrot2AudioPreTrainedModel(PreTrainedModel):
+#     config_class = Parrot2AudioConfig
+#     # TODO: check this prefix
+#     base_model_prefix = "model"
+#     supports_gradient_checkpointing = True
+#     _skip_keys_device_placement = "past_key_values"
+#     _supports_flash_attn_2 = True
+#     _supports_sdpa = True
+#     _supports_attention_backend = False
 
-    This model is also a PyTorch [torch.nn.Module](https://pytorch.org/docs/stable/nn.html#torch.nn.Module) subclass.
-    Use it as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to general usage
-    and behavior.
+#     def _init_weights(self, module):
+#         # important: this ported version of Parrot2Audio isn't meant for training from scratch - only
+#         # inference and fine-tuning - so the proper init weights code has been removed
+#         std = self.config.init_std if hasattr(self.config, "init_std") else self.config.audio_config.init_std
 
-    Parameters:
-        config ([`Parrot2AudioEncoderConfig`]):
-            Model configuration class with all the parameters of the model. Initializing with a config file does not
-            load the weights associated with the model, only the configuration. Check out the
-            [`~PreTrainedModel.from_pretrained`] method to load the model weights.
-"""
-
-
-# TODO: add comment to hint this class is copied from ParrotSenseVoiceEncoder
-@add_start_docstrings(
-    """The audio model from Parrot2Audio without any head or projection on top.""",
-    PARROT2AUDIOENCODER_START_DOCSTRING,
-)
-class Parrot2AudioEncoder(Parrot2AudioPreTrainedModel):
-    """
-    Transformer encoder consisting of *config.encoder_layers* self attention layers. Each layer is a
-    [`Parrot2AudioEncoderLayer`].
-
-    Args:
-        config: Parrot2AudioEncoderConfig
-    """
-
-    # Ignore copy
-    config_class = Parrot2AudioEncoderConfig
-    main_input_name = "input_features"
-    _no_split_modules = ["EncoderLayerSANM"]
-
-    def __init__(self, config: Parrot2AudioEncoderConfig):
-        super().__init__(config)
-        self.sense_voice_small = SenseVoiceEncoderSmall(
-            input_size=config.input_size,
-            output_size=config.output_size,
-            attention_heads=config.attention_heads,
-            linear_units=config.linear_units,
-            num_blocks=config.num_blocks,
-            tp_blocks=config.tp_blocks,
-            dropout_rate=config.dropout_rate,
-            attention_dropout_rate=config.attention_dropout_rate,
-            normalize_before=config.normalize_before,
-            kernel_size=config.kernel_size,
-            sanm_shfit=config.sanm_shfit
-        )
-
-    def _freeze_parameters(self):
-        for param in self.parameters():
-            param.requires_grad = False
-        self._requires_grad = False
-
-    # TODO: refine output type to BaseModelOutput
-    def forward(
-        self,
-        input_features: torch.Tensor,
-        attention_mask: Optional[torch.Tensor] = None,
-        audio_feature_lengths: Optional[torch.Tensor] = None,
-        head_mask: Optional[torch.Tensor] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
-        return_dict: Optional[bool] = None,
-    ) -> tuple[torch.Tensor, torch.Tensor]:
-        xs_pad, olens, _ = self.sense_voice_small(
-            input_features,
-            ilens = audio_feature_lengths
-        )
-        return xs_pad, olens
-
-    # Ignore copy
-    def _get_feat_extract_output_lengths(self, input_lengths: torch.LongTensor) -> tuple[torch.LongTensor, torch.LongTensor]:
-        """
-        Computes the output length of the parrot2 audio encoder
-        """
-        return input_lengths, input_lengths
+#         if isinstance(module, (nn.Linear, nn.Conv1d)):
+#             module.weight.data.normal_(mean=0.0, std=std)
+#             if module.bias is not None:
+#                 module.bias.data.zero_()
+#         elif isinstance(module, nn.Embedding):
+#             module.weight.data.normal_(mean=0.0, std=std)
+#             if module.padding_idx is not None:
+#                 module.weight.data[module.padding_idx].zero_()
 
 
-class LinearAdaptor(nn.Module):
-    def __init__(self, encoder_dim: int, ffn_dim: int, llm_dim: int, **kwargs):
-        super().__init__()
-        self.linear1 = nn.Linear(encoder_dim, ffn_dim)
-        self.relu = nn.ReLU()
-        self.linear2 = nn.Linear(ffn_dim, llm_dim)
-        self.final_norm = nn.LayerNorm(llm_dim)
+# PARROT2AUDIOENCODER_START_DOCSTRING = r"""
+#     This model inherits from [`PreTrainedModel`]. Check the superclass documentation for the generic methods the
+#     library implements for all its model (such as downloading or saving, resizing the input embeddings, pruning heads
+#     etc.)
 
-    def forward(self, x: torch.Tensor, **kwargs) -> tuple[torch.Tensor, torch.Tensor]:
-        x = self.linear1(x)
-        x = self.relu(x)
-        x = self.linear2(x)
-        x = self.final_norm(x)
-        return x, torch.tensor([x.shape[1]], dtype=torch.int32, device=x.device)
+#     This model is also a PyTorch [torch.nn.Module](https://pytorch.org/docs/stable/nn.html#torch.nn.Module) subclass.
+#     Use it as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to general usage
+#     and behavior.
+
+#     Parameters:
+#         config ([`Parrot2AudioEncoderConfig`]):
+#             Model configuration class with all the parameters of the model. Initializing with a config file does not
+#             load the weights associated with the model, only the configuration. Check out the
+#             [`~PreTrainedModel.from_pretrained`] method to load the model weights.
+# """
+
+
+# # TODO: add comment to hint this class is copied from ParrotSenseVoiceEncoder
+# @add_start_docstrings(
+#     """The audio model from Parrot2Audio without any head or projection on top.""",
+#     PARROT2AUDIOENCODER_START_DOCSTRING,
+# )
+# class Parrot2AudioEncoder(Parrot2AudioPreTrainedModel):
+#     """
+#     Transformer encoder consisting of *config.encoder_layers* self attention layers. Each layer is a
+#     [`Parrot2AudioEncoderLayer`].
+
+#     Args:
+#         config: Parrot2AudioEncoderConfig
+#     """
+
+#     # Ignore copy
+#     config_class = Parrot2AudioEncoderConfig
+#     main_input_name = "input_features"
+#     _no_split_modules = ["EncoderLayerSANM"]
+
+#     def __init__(self, config: Parrot2AudioEncoderConfig):
+#         super().__init__(config)
+#         self.sense_voice_small = SenseVoiceEncoderSmall(
+#             input_size=config.input_size,
+#             output_size=config.output_size,
+#             attention_heads=config.attention_heads,
+#             linear_units=config.linear_units,
+#             num_blocks=config.num_blocks,
+#             tp_blocks=config.tp_blocks,
+#             dropout_rate=config.dropout_rate,
+#             attention_dropout_rate=config.attention_dropout_rate,
+#             normalize_before=config.normalize_before,
+#             kernel_size=config.kernel_size,
+#             sanm_shfit=config.sanm_shfit
+#         )
+
+#     def _freeze_parameters(self):
+#         for param in self.parameters():
+#             param.requires_grad = False
+#         self._requires_grad = False
+
+#     # TODO: refine output type to BaseModelOutput
+#     def forward(
+#         self,
+#         input_features: torch.Tensor,
+#         attention_mask: Optional[torch.Tensor] = None,
+#         audio_feature_lengths: Optional[torch.Tensor] = None,
+#         head_mask: Optional[torch.Tensor] = None,
+#         output_attentions: Optional[bool] = None,
+#         output_hidden_states: Optional[bool] = None,
+#         return_dict: Optional[bool] = None,
+#     ) -> tuple[torch.Tensor, torch.Tensor]:
+#         xs_pad, olens, _ = self.sense_voice_small(
+#             input_features,
+#             ilens = audio_feature_lengths
+#         )
+#         return xs_pad, olens
+
+#     # Ignore copy
+#     def _get_feat_extract_output_lengths(self, input_lengths: torch.LongTensor) -> tuple[torch.LongTensor, torch.LongTensor]:
+#         """
+#         Computes the output length of the parrot2 audio encoder
+#         """
+#         return input_lengths, input_lengths
+
+
+Parrot2AudioPreTrainedModel = ParrotSenseVoicePreTrainedModel
+Parrot2AudioEncoder = ParrotSenseVoiceEncoder
 
 
 class Parrot2AudioMultiModalProjector(nn.Module):
