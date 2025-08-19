c8d864dec84a5861c9b6f843a240857964a60910
Monad Kai <onlookerliu_sjtu@163.com>
2025-08-19 03:25:10 +0000

    :new: Register parrot models

--- src/transformers/models/auto/feature_extraction_auto.py
+++ src/transformers/models/auto/feature_extraction_auto.py
@@ -81,6 +81,7 @@ FEATURE_EXTRACTOR_MAPPING_NAMES = OrderedDict(
         ("moshi", "EncodecFeatureExtractor"),
         ("nat", "ViTFeatureExtractor"),
         ("owlvit", "OwlViTFeatureExtractor"),
+        ("parrot_audio", "ParrotAudioFeatureExtractor"),
         ("perceiver", "PerceiverFeatureExtractor"),
         ("phi4_multimodal", "Phi4MultimodalFeatureExtractor"),
         ("poolformer", "PoolFormerFeatureExtractor"),
